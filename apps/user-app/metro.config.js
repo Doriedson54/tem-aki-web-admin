e077fdf023adfd7658c7eb839b243853d015541f
Dodo110371 <dsdodo18@yahoo.com.br>
2026-04-22 19:10:35 -0300

    fix(android apk): build release apk and align node/cli deps

--- apps/user-app/metro.config.js
+++ apps/user-app/metro.config.js
@@ -1,5 +1,4 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config');
 
-const config = getDefaultConfig(__dirname);
+module.exports = getDefaultConfig(__dirname);
 
-module.exports = config;
